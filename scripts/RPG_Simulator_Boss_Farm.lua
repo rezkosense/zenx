05c88f43539b854e7a052ffd73565e23181ead2c
laderite <78981916+laderite@users.noreply.github.com>
2021-12-31 02:18:30 -0800

    Update RPG_Simulator_Boss_Farm.lua

--- scripts/RPG_Simulator_Boss_Farm.lua
+++ scripts/RPG_Simulator_Boss_Farm.lua
@@ -1,17 +1,21 @@
 repeat wait() until game:IsLoaded()
 repeat wait() until game:GetService("Players").LocalPlayer:WaitForChild('Loaded').Value == true
 repeat wait() until (#game:GetService("Workspace"):WaitForChild('Mobs'):GetChildren()) > 0
-if not workspace:FindFirstChild('W1') then
-getgenv().drink = true
-getgenv().autoskill1 = true
-getgenv().autoskill2 = true
-getgenv().autoskill3 = true
-else
-    getgenv().drink = false
-getgenv().autoskill1 = false
-getgenv().autoskill2 = false
-getgenv().autoskill3 = false
-end
+
+-- settings
+getgenv().raidsettings = {
+    ["autodrinkpotions"] = true,
+    ["farmsettings"] = {
+        ["autoswing"] = true,
+        ["range"] = 10
+    },
+    ["autoskill"] = {
+        ["autoskill1"] = true,
+        ["autoskill2"] = true,
+        ["autoskill3"] = true
+    },
+}
+
 local VirtualInputManager = game:GetService('VirtualInputManager')
 function swingdasword()
     local args = {[1] = "Slash"}
@@ -31,30 +35,30 @@ function lookAt(chr,target) -- found this func somewhere
 end
 function farmraid()
     if not workspace:FindFirstChild('W1') and not workspace:FindFirstChild('QuestNPCs') then
-            part = game:GetService("Workspace"):WaitForChild('Mobs'):FindFirstChild('Crystal') or game:GetService("Workspace"):WaitForChild('Mobs'):FindFirstChild('Stand') or game:GetService("Workspace"):WaitForChild('Mobs'):FindFirstChildWhichIsA("Model")
-            if part then
-                swing = true
-                repeat
-                    game.Players.LocalPlayer.Character:WaitForChild('HumanoidRootPart').CFrame = part.HumanoidRootPart.CFrame + Vector3.new(0,0,3)
-                    lookAt(game.Players.LocalPlayer.Character, part.HumanoidRootPart)
-                    wait()
-                until abort or part.Humanoid.Health == 0 or not part:IsDescendantOf(game.Workspace.Mobs) or not part.HumanoidRootPart:IsDescendantOf(part) or game.Players.LocalPlayer.Character.Humanoid.Health == 0
-                swing = false
-                abort = false
-                farmraid()
-            else
-                wait()
-                farmraid()
-                end
+        part = game:GetService("Workspace"):WaitForChild('Mobs'):FindFirstChild('Crystal') or 
+        game:GetService("Workspace"):WaitForChild('Mobs'):FindFirstChild('Stand') or 
+        game:GetService("Workspace"):WaitForChild('Mobs'):FindFirstChildWhichIsA("Model")
+        if part then
+            if getgenv().raidsettings['farmsettings']['autoswing'] then swing = true end
+            repeat
+                game.Players.LocalPlayer.Character:WaitForChild('HumanoidRootPart').CFrame = part.HumanoidRootPart.CFrame + Vector3.new(0,0,getgenv().raidsettings['farmsettings']['range'])
+                lookAt(game.Players.LocalPlayer.Character, part.HumanoidRootPart)
+                task.wait()
+            until abort or part.Humanoid.Health == 0 or not part:IsDescendantOf(game.Workspace.Mobs) or not part.HumanoidRootPart:IsDescendantOf(part) or game.Players.LocalPlayer.Character.Humanoid.Health == 0
+            swing = false
+            abort = false
+            farmraid()
+        else
+            wait()
+            farmraid()
+        end
     end
 end
 game.Players.LocalPlayer.CharacterAdded:Connect(function()
-    if not workspace:FindFirstChild('W1') then -- see if we are in a raid or not
-        farmraid()
-    end
+    farmraid()
 end)
 workspace:WaitForChild('Mobs').ChildAdded:Connect(function(v)
-    if v.Name == "Stand" then
+    if v.Name == "Stand" or "Crystal"then
         abort = true
         farmraid()
     end
@@ -67,36 +71,30 @@ coroutine.resume(coroutine.create(function()
         end
     end        
 end))
+
 coroutine.resume(coroutine.create(function()
     while wait(1) do
-        if getgenv().autoskill1 then
+        if getgenv().raidsettings['autoskill']['autoskill1'] and not workspace:FindFirstChild('W1') and not workspace:FindFirstChild('QuestNPCs') then
             VirtualInputManager:SendKeyEvent(true, "Q", false, game)
             wait()
             VirtualInputManager:SendKeyEvent(false, "Q", false, game)
         end
-    end
-end))
-coroutine.resume(coroutine.create(function()
-    while wait(1) do
-        if getgenv().autoskill2 then
+        if getgenv().raidsettings['autoskill']['autoskill2'] and not workspace:FindFirstChild('W1') and not workspace:FindFirstChild('QuestNPCs') then
             VirtualInputManager:SendKeyEvent(true, "E", false, game)
             wait()
             VirtualInputManager:SendKeyEvent(false, "E", false, game)
         end
-    end
-end))
-coroutine.resume(coroutine.create(function()
-    while wait(1) do
-        if getgenv().autoskill3 then
+        if getgenv().raidsettings['autoskill']['autoskill13'] and not workspace:FindFirstChild('W1') and not workspace:FindFirstChild('QuestNPCs') then
             VirtualInputManager:SendKeyEvent(true, "R", false, game)
             wait()
             VirtualInputManager:SendKeyEvent(false, "R", false, game)
         end
     end
 end))
+
 coroutine.resume(coroutine.create(function()
     while wait(1) do
-        if getgenv().drink then
+        if getgenv().raidsettings['autodrinkpotions'] then 
             game:GetService("ReplicatedStorage").Events.drink:FireServer()
         end
     end
